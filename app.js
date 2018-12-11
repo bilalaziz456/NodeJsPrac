f62beb216ff0d6f6210a0b670fa10c6db2d8e98f
Bilal <bilalaziz456@gmail.com>
2018-12-11 15:20:21 +0500

    message

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-var person = {
-    firstname : 'John',
-    lastname : 'Doe',
-    greet : function(){
-        console.log("Hello " + this.firstname + " " + this.lastname);
-    }
-}
+function Person (firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+};
+Person.prototype.greet = function(){
+    console.log("Hello "+ this.firstname + " " + this.lastname)
+};
 
-person.greet();
+Person.prototype.address = "Johar";
+
+let name = new Person('John', 'Doe');
+name.greet();
+console.log(name.address);
